fca8cd618549b7f05068fdb29e2f7dbe2c7c476d
DESKTOP-OVL581Q\pc <kysuk05@gmail.com>
2022-02-15 22:56:15 +0900

    BOJ 브론즈 난이도 2문제 업로드

---
+++ 브론즈/BJO8370.py
@@ -0,0 +1,11 @@
+# Byteland Airlines recently extended their aircraft fleet with a new model of a plane. The new acquisition has n1 rows of seats in the business class and n2 rows in the economic class. In the business class each row contains k1 seats, while each row in the economic class has k2 seats.
+
+# Write a program which:
+
+# reads information about available seats in the plane,
+# calculates the sum of all seats available in that plane,
+# writes the result.
+
+
+a,b,c,d = map(int,input().split())
+print(a*b+c*d)
